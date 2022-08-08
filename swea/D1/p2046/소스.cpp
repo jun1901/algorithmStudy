c1d54c4bcaf1d1c4d09e2191ab5a95350cdfe405
lee hakjun <gkrwns1901@naver.com>
2022-08-08 19:45:45 +0900

    first code

---
+++ swea/D1/p2046/소스.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie();
+	cout.tie();
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cout << "#";
+	
+
+	return 0;
+}
